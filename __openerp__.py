f08a3649be719448a82b1c589ff3f9223353a57c
aliomattux <kyle.waid@gcotech.com>
2014-12-07 20:01:54 -0600

    Add dependency on stock_package module

--- __openerp__.py
+++ __openerp__.py
@@ -3,7 +3,7 @@
     'version': '1.1',
     'author': 'Kyle Waid',
     'category': 'Sales Management',
-    'depends': ['delivery'],
+    'depends': ['stock_package'],
     'website': 'https://www.gcotech.com',
     'description': """ 
     """,
