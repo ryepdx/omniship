b699b08787e2253b7e59e8473d3573a19d362d49
Ryan Casey <ryepdx@gmail.com>
2015-02-16 16:19:55 -0800

    Override get_price to return the quoted shipping price if it's set.

--- models/delivery.py
+++ models/delivery.py
@@ -7,3 +7,19 @@ class DeliveryCarrier(osv.osv):
         'quoted_price': fields.float('Quoted Price', help="Price retrieved from shipping API."),
         'sale_id': fields.many2one('sale.order', 'Sale Order', help='Sale order this quote was retrieved for.')
     }
+
+    def get_price(self, cr, uid, ids, field_name, arg=None, context=None):
+        res = {}
+        for carrier in self.browse(cr, uid, ids, context=context):
+            if not carrier.quoted_price and not carrier.sale_id:
+                continue
+
+            res[carrier.id] = {
+                'price': carrier.quoted_price,
+                'available': True
+            }
+
+        res.update(super(DeliveryCarrier, self).get_price(
+            cr, uid, list(set(ids) - set(res.keys())), field_name, arg=arg, context=context))
+
+        return res
