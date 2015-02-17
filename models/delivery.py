9fad2980ff8fb2fceb92a4a8882a09feeed61759
Ryan Casey <ryepdx@gmail.com>
2015-02-16 16:11:32 -0800

    Add columns to support dynamic shipping rate fetching.

--- models/delivery.py
+++ models/delivery.py
@@ -2,4 +2,8 @@ from openerp.osv import osv, fields
 
 class DeliveryCarrier(osv.osv):
     _inherit = 'delivery.carrier'
-    _columns = {'service': fields.many2one('delivery.carrier.service', 'Service')}
+    _columns = {
+        'service': fields.many2one('delivery.carrier.service', 'Service'),
+        'quoted_price': fields.float('Quoted Price', help="Price retrieved from shipping API."),
+        'sale_id': fields.many2one('sale.order', 'Sale Order', help='Sale order this quote was retrieved for.')
+    }
