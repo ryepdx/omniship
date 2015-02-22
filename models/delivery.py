16bc97b40aec3e860201e98229697cc62fbab804
Ryan Casey <ryepdx@gmail.com>
2015-02-22 15:42:50 -0800

    Remove unnecessary 'quoted_price' column.

--- models/delivery.py
+++ models/delivery.py
@@ -4,18 +4,17 @@ class DeliveryCarrier(osv.osv):
     _inherit = 'delivery.carrier'
     _columns = {
         'service': fields.many2one('delivery.carrier.service', 'Service'),
-        'quoted_price': fields.float('Quoted Price', help="Price retrieved from shipping API."),
         'sale_id': fields.many2one('sale.order', 'Sale Order', help='Sale order this quote was retrieved for.')
     }
 
     def get_price(self, cr, uid, ids, field_name, arg=None, context=None):
         res = {}
         for carrier in self.browse(cr, uid, ids, context=context):
-            if not carrier.quoted_price and not carrier.sale_id:
+            if not carrier.sale_id:
                 continue
 
             res[carrier.id] = {
-                'price': carrier.quoted_price,
+                'price': carrier.normal_price,
                 'available': True
             }
 
