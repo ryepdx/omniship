de8fd884d5b819a9a615ccba8e171ff7d7be2636
Ryan Casey <ryepdx@gmail.com>
2015-02-23 12:32:55 -0800

    Improved forward-compatibility with PyUPS 0.6.1

--- models/ups_api.py
+++ models/ups_api.py
@@ -1,5 +1,4 @@
 import base64
-from ups import ShipmentConfirm, ShipmentAccept, ShipmentVoid, PyUPSException
 from openerp.osv import osv, fields
 from lxml import etree
 from lxml.builder import E
@@ -9,6 +8,12 @@ import cStringIO
 from cStringIO import StringIO
 from tempfile import NamedTemporaryFile
 import subprocess
+
+try:
+    from ups import ShipmentConfirm, ShipmentAccept, ShipmentVoid, PyUPSException
+except ImportError:
+    from ups.shipping_package import ShipmentConfirm, ShipmentAccept, ShipmentVoid
+    from ups.base import PyUPSException
 
 class OmnishipProcessor(osv.osv_memory):
     _inherit = 'omniship.processor'
